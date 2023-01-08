609de20a31cf0afcfe7dc2d4a39066ebcdebfaed
Investe USA <gustavocarvalhodesousa@gmail.com>
2023-01-08 12:41:01 -0300

    test: Must not apply an expired discount coupon

--- domain/entity/order_test.go
+++ domain/entity/order_test.go
@@ -2,6 +2,7 @@ package entity
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -45,7 +46,7 @@ func TestNewOrderWithProducts(t *testing.T) {
 	total := OrderGetTotal(o)
 	assert.Equal(t, total, float64(2030))
 }
-func TestNewOrderWithProductsWithCoupon(t *testing.T) {
+func TestNewOrderWithProductsWithValidCoupon(t *testing.T) {
 	product1 := Product{
 		Id:          "1",
 		Name:        "Guitarra",
@@ -87,6 +88,51 @@ func TestNewOrderWithProductsWithCoupon(t *testing.T) {
 	o, _ := fakeOrderWithCoupom(items, coupon)
 	total := OrderGetTotal(o)
 	assert.Equal(t, total, float64(1624))
+}
+
+func TestNewOrderWithProductNotApplyExpiredCoupon(t *testing.T) {
+	product1 := Product{
+		Id:          "1",
+		Name:        "Guitarra",
+		Description: "Guitarra elétrica",
+	}
+	product2 := Product{
+		Id:          "2",
+		Name:        "Cabo P2",
+		Description: "Cabo de som",
+	}
+	product3 := Product{
+		Id:          "3",
+		Name:        "Paleta",
+		Description: "Paleta de acrílico",
+	}
+	i1 := Item{
+		Id:       "idItem1",
+		Price:    2000,
+		Quantity: 1,
+		Product:  product1,
+	}
+	i2 := Item{
+		Id:       "idItem2",
+		Price:    20,
+		Quantity: 1,
+		Product:  product2,
+	}
+	i3 := Item{
+		Id:       "idItem3",
+		Price:    10,
+		Quantity: 1,
+		Product:  product3,
+	}
+	items := []Item{i1, i2, i3}
+	coupon := Coupon{
+		Name:        "VALE20",
+		Value:       20,
+		ExpiredDate: time.Date(2022, 12, 10, 23, 0, 0, 0, time.UTC),
+	}
+	o, _ := fakeOrderWithCoupom(items, coupon)
+	total := OrderGetTotal(o)
+	assert.Equal(t, total, float64(2030))
 }
 
 func fakeOrder(items []Item) (*Order, error) {
